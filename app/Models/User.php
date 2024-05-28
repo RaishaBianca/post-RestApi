e34a875c32c91c6c2ee9be7d77e7260a5ada4db5
RaishaBianca <biancacaca489@gmail.com>
2024-05-28 16:49:57 +0700

    add auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Postings;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
     protected $connection = 'mysql_user';
     protected $table = 'users';
 
--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::group(['prefix' => 'users'], function () {
-    Route::get('/', [UserController::class, 'index']);
     Route::post('/create', [UserController::class, 'store']);
-    Route::post('/login', [UserController::class, 'login']);
-    Route::put('/update', [UserController::class, 'update']);
-    Route::delete('/delete', [UserController::class, 'delete']);
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::put('/update', [UserController::class, 'update']);
+        Route::delete('/delete', [UserController::class, 'delete']);
+    });
 });
 
 Route::group(['prefix' => 'posts'], function () {
@@ -18,4 +21,9 @@ Route::group(['prefix' => 'posts'], function () {
     Route::put('/update', [PostController::class, 'update']);
     Route::delete('/delete', [PostController::class, 'delete']);
     Route::get('/author', [PostController::class, 'getByAuthor']);
+});
+
+
+Route::fallback(function () {
+    return response()->json(['message' => 'Unauthorized.'], 404);
 });
